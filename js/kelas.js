ba37c11bae3a1d22d97e534ea9434cc36fd231e6
muhammadarsilalhabsy <muhammadarsilalhabsy@gmail.com>
2022-10-23 11:19:36 +0800

    api function, fix bugs di caraousel home

--- js/kelas.js
+++ js/kelas.js
@@ -1,56 +1,45 @@
-let API = "https://635272a4ffbb7da0f2d8983d.mockapi.io/api/v1/ucup"
+let API = 'https://635272a4ffbb7da0f2d8983d.mockapi.io/api/v1/ucup'
 
 
-let getInnerCarousel = document.querySelector(".carousel-inner");
-// let getBtnMelukis = document.getElementById("btn-melukis");
-// let getBtnDigitalArt = document.getElementById("btn-digital-art");
-// let getBtnTeater = document.getElementById("btn-teater");
-// let getBtnMenari = document.getElementById("btn-menari");
+
+let getInnerCarousel = document.querySelector(".carousel-inner")
+
+const getApi = async () => {
+  let res = await fetch(API);
+    let object = await res.json();
+    let carousel_item = ``;
+    console.info(object)
+    object.forEach(element => carousel_item += displayClass(element));
+    getInnerCarousel.innerHTML = carousel_item;
+    console.info(getInnerCarousel);
+    const addActive = document.getElementsByClassName("carousel-item")[0];
+    addActive.classList.add("active");
+}
+
+
 
 // display
 function displayClass (item) {
-  return `
-  <div class="carousel-item">
-    <div class="container my-5">
-      <div class="row g-0 position-relative">
-        <div class="col-md-6 mb-md-0 p-md-4">
-          <img src="${item.img}" class="banner" alt="..." />
-        </div>
-        <div class="col-md-6 p-4 ps-md-0">
-          <h5 class="mt-3 fw-bold">${item.judul}</h5>
-          <p class="text-secondary">${item.caption}</p>
-          <a href="#" class="text-decoration-none">Cari tahu lebih lanjut >></a>
-        </div>
-      </div> 
-    </div>
-  <div class="carousel-item">
-  `
-}
-
-fetch(API)
-.then(response => {response.json()
-  console.info(response);
-})
-.then(object => {        
-    getInnerCarousel.innerHTML = showClass(object)
-    console.info(object);
-}).catch(e => {
-  console.info(e);
-})
-
-
-function showClass(arr){
-  let carousel_item = '';
-  arr.forEach(element => {
-    carousel_item += displayClass(element);
-  });
+  return `<div class="carousel-item">
+            <div class="row g-0 position-relative">
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="img/image 17.png" class="banner" alt="..." />
+              </div>
+              <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-3 fw-bold">${item.judul}</h5>
+                <p class="text-secondary">${item.caption}</p>
+                <a href="#" class="text-decoration-none"
+                  >Cari tahu lebih lanjut >></a
+                >
+              </div>
+            </div>
+          </div>`
 }
 
 
 
 
-
-
+getApi();
 
 
 
