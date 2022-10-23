9ab12b9f2ec058d8da340a3301d7d87e9783b3dc
muhammadarsilalhabsy <muhammadarsilalhabsy@gmail.com>
2022-10-23 21:05:00 +0800

    menghapus img, dan menambahkan html baru

--- js/general_class.js
+++ js/general_class.js
@@ -20,10 +20,10 @@ const getApi = async () => {
 
 // display
 function displayClass (item) {
-  return `<div class="carousel-item">
+  return `<div class="carousel-item my-5">
             <div class="row g-0 position-relative">
               <div class="col-md-6 mb-md-0 p-md-4">
-                <img src="img/image 17.png" class="banner" alt="..." />
+                <img src="${item.img}" class="banner" alt="..." />
               </div>
               <div class="col-md-6 p-4 ps-md-0">
                 <h5 class="mt-3 fw-bold">${item.judul}</h5>
