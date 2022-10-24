a77abd5d1d4ff61face51d0e85780a8c7c2f77cf
muhammadarsilalhabsy <muhammadarsilalhabsy@gmail.com>
2022-10-24 08:45:15 +0800

    fix avatar popup

--- js/general_class.js
+++ js/general_class.js
@@ -38,37 +38,5 @@ function displayClass (item) {
 }
 
 
-
-
+// call function
 getApi();
-
-
-
-
-
-
-
-
-
-
-/* <div class="carousel-item">
-<div class="container my-5">
-  
-  <div class="row g-0 position-relative">
-    <div class="col-md-6 mb-md-0 p-md-4">
-      <img src="img/image 17.png" class="banner" alt="..." />
-    </div>
-    <div class="col-md-6 p-4 ps-md-0">
-      <h5 class="mt-3 fw-bold">Columns with stretched link</h5>
-      <p class="text-secondary">
-        Another instance of placeholder content for this other custom
-        component. It is intended to mimic what some real-world content
-        would look like, and we're using it here to give the component a
-        bit of body and size.
-      </p>
-      <a href="#" class="text-decoration-none"
-        >Cari tahu lebih lanjut >></a
-      >
-    </div>
-  </div>
-</div> */
