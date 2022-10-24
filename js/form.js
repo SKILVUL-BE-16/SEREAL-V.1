cdded3e3ad23c040e7ea20719883f113ac6f7db1
muhammadarsilalhabsy <muhammadarsilalhabsy@gmail.com>
2022-10-24 22:56:24 +0800

    fix register

--- js/form.js
+++ js/form.js
@@ -29,19 +29,19 @@ function login() {
 }
 
 // check if register
-const getCheckProfile = document.getElementById("check-profile");
+// const getCheckProfile = document.getElementById("check-profile");
 
-let getRegistAccount = JSON.parse(localStorage.getItem("Regist_Account"));
-console.info(getRegistAccount);
-if (getRegistAccount.email !== undefined || getRegistAccount.email !== null){
-    getCheckProfile.innerHTML = `<div class="test-circle navbar-nav gap-1 gap-md-4 mx"></div>`;
-}else{
-    getCheckProfile.innerHTML = 
-        `<ul class="navbar-nav gap-1 gap-md-4 mx">
-            <li class="nav-item">
-                <a class="nav-link reg fancy-link" href="./register.html">Register</a>
-            </li>
-                <a href="./login.html"><button class="btn btn-main-color">Login</button></a>
-        </ul>`;
-}
+// let getRegistAccount = JSON.parse(localStorage.getItem("Regist_Account"));
+// console.info(getRegistAccount);
+// if (getRegistAccount.email !== undefined || getRegistAccount.email !== null){
+//     getCheckProfile.innerHTML = `<div class="test-circle navbar-nav gap-1 gap-md-4 mx"></div>`;
+// }else{
+//     getCheckProfile.innerHTML = 
+//         `<ul class="navbar-nav gap-1 gap-md-4 mx">
+//             <li class="nav-item">
+//                 <a class="nav-link reg fancy-link" href="./register.html">Register</a>
+//             </li>
+//                 <a href="./login.html"><button class="btn btn-main-color">Login</button></a>
+//         </ul>`;
+// }
 
